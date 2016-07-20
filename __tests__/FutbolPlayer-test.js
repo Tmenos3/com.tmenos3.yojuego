c05703ddeecc211bef8ae3dd89dfb4d91c697c11
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2016-07-19 23:46:43 -0300

    .

--- __tests__/FutbolPlayer-test.js
+++ __tests__/FutbolPlayer-test.js
@@ -1,15 +1,17 @@
-jest.unmock('../src/model/FutbolPlayer');
+import FutbolPlayer from '../src/model/FutbolPlayer';
+
+jest.dontmock('../src/model/FutbolPlayer');
 
 describe('FutbolPlayer', () => {
   it('create a FutbolPlayer with a name', () => {
     const name = "aName";
-    const fp = new require('../src/model/FutbolPlayer')(name);
+    const fp = new FutbolPlayer(name);
     expect(fp.getName()).toBe(name);
   });
 
   it('create a FutbolPlayer with a city', () => {
     const city = "aCity";
-    const fp = new require('../src/model/FutbolPlayer')("aName", city);
+    const fp = new FutbolPlayer("aName", city);
     expect(fp.getCity()).toBe(city);
   });
 });
