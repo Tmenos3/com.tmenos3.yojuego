0625958aa2b431f0c05bbdb6fa3003ff27d7e8d6
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2016-11-09 23:42:34 -0300

    Maquetado del componente MatchDetailBody

--- src/components/match/MatchDetailBody.js
+++ src/components/match/MatchDetailBody.js
@@ -2,43 +2,58 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     View,
+    TextInput,
     Text,
     TouchableOpacity,
-    Image
+    Image,
+    Dimensions,
+    ListView
 } from 'react-native';
 
 class MatchDetailBody extends Component {
     constructor(props) {
         super(props);
 
-        // const dsComments = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-        // const dsPlayers = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-        // this.state = {
-        //     comments: dsComments.cloneWithRows([
-        //         { id: 1, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 2, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 3, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 4, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 5, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 6, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 7, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 8, date: { day: '21', month: 'OCT' }, desc: 'One match.' }]),
-        //     players: dsPlayers.cloneWithRows([
-        //         { id: 1, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 2, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 3, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 4, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 5, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 6, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 7, date: { day: '21', month: 'OCT' }, desc: 'One match.' },
-        //         { id: 8, date: { day: '21', month: 'OCT' }, desc: 'One match.' }]),
-        //     loadingMatch: false
-        // };
+        const dsComments = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        const dsPlayersAccepted = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        const dsPlayersWaiting = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.state = {
+            comments: dsComments.cloneWithRows([
+                { id: 1, comment: 'This is a comment sent by me' },
+                { id: 2, comment: 'a comment' },
+                { id: 3, comment: 'a comment' },
+                { id: 1, comment: 'This is another comment sent by me' },
+                { id: 5, comment: 'a comment' },
+                { id: 6, comment: 'a comment' },
+                { id: 1, comment: 'Sent by me too' },
+                { id: 8, comment: 'a comment' }]),
+            playersAccepted: dsPlayersAccepted.cloneWithRows([
+                { id: 1, date: 'algo' },
+                { id: 2, date: 'algo' },
+                { id: 3, date: 'algo' },
+                { id: 4, date: 'algo' },
+                { id: 5, date: 'algo' },
+                { id: 6, date: 'algo' },
+                { id: 7, date: 'algo' },
+                { id: 8, date: 'algo' }]),
+            playersWaiting: dsPlayersWaiting.cloneWithRows([
+                { id: 1, date: 'algo' },
+                { id: 2, date: 'algo' },
+                { id: 3, date: 'algo' },
+                { id: 4, date: 'algo' },
+                { id: 5, date: 'algo' },
+                { id: 6, date: 'algo' },
+                { id: 7, date: 'algo' },
+                { id: 8, date: 'algo' }]),
+            loadingMatch: false,
+            inputHeight: 30
+        };
 
-        this._matchDetailComments = this._matchDetailComments.bind(this);
-        this._matchDetailPlayers = this._matchDetailPlayers.bind(this);
         this._renderRowComments = this._renderRowComments.bind(this);
-        this._renderRowPlayers = this._renderRowPlayers.bind(this);
+        this._renderRowPlayersAccepted = this._renderRowPlayersAccepted.bind(this);
+        this._renderRowPlayersWaiting = this._renderRowPlayersWaiting.bind(this);
+        this._renderPlayers = this._renderPlayers.bind(this);
+        this._renderComments = this._renderComments.bind(this);
     }
 
     componentDidMount() {
@@ -52,90 +67,91 @@ class MatchDetailBody extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.panelLeft}>
-                    <View style={styles.sectionLeft}>
-                    </View>
-                    <View style={styles.sectionLeft}>
-                    </View>
+                {this._renderPlayers()}
+                {this._renderComments()}
+            </View>
+        );
+    }
+
+    _renderPlayers() {
+        return (
+            <View style={styles.sectionPlayers}>
+                <ListView
+                    dataSource={this.state.playersAccepted}
+                    renderRow={this._renderRowPlayersAccepted}
+                    style={styles.listViewPlayers}
+                    horizontal={true}
+                    />
+                <ListView
+                    dataSource={this.state.playersWaiting}
+                    renderRow={this._renderRowPlayersWaiting}
+                    style={styles.listViewPlayers}
+                    horizontal={true}
+                    />
+            </View>
+        );
+    }
+
+    _renderComments() {
+        return (
+            <View style={styles.sectionMessages}>
+                <View style={styles.sectionForReading}>
+                    <ListView
+                        dataSource={this.state.comments}
+                        renderRow={this._renderRowComments}
+                        style={styles.listView}
+                        />
                 </View>
-                <View style={styles.panelRight}>
-                    <View style={styles.sectionRight}>
-                        <TouchableOpacity style={styles.button}>
-                            <Image style={styles.buttonImage} source={require('../../statics/add-player.png')}></Image>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.sectionRight}>
-                        <TouchableOpacity style={styles.button}>
-                            <Image style={styles.buttonImage} source={require('../../statics/write-message.png')}></Image>
-                        </TouchableOpacity>
-                    </View>
+                <View style={styles.sectionForWritting}>
+                    <TextInput style={styles.sendComment} placeholder={"Escribir..."}>
+                    </TextInput>
+                    <TouchableOpacity style={styles.button}>
+                        <Image style={styles.buttonImage} source={require('../../statics/write-message.png')}></Image>
+                    </TouchableOpacity>
                 </View>
             </View>
         );
     }
 
-    _matchDetailComments() {
-        // return (
-        //     <View style={styles.container}>
-        //         <ListView
-        //             dataSource={this.state.comments}
-        //             renderRow={this._renderRowComments}
-        //             style={styles.listView}
-        //             />
-        //         <TouchableOpacity style={styles.button} onPress={this._showSignUp}>
-        //             <Text style={styles.buttonText}>+</Text>
-        //         </TouchableOpacity>
-        //         {this._renderLoading()}
-        //     </View>
-        // );
-    }
-
-    _matchDetailPlayers() {
-        // return (
-        //     <View style={styles.container}>
-        //         <ListView
-        //             dataSource={this.state.players}
-        //             renderRow={this._renderRowPlayers}
-        //             style={styles.listView}
-        //             />
-        //         <TouchableOpacity style={styles.button} onPress={this._showSignUp}>
-        //             <Text style={styles.buttonText}>+</Text>
-        //         </TouchableOpacity>
-        //         {this._renderLoading()}
-        //     </View>
-        // );
-    }
-
     _renderRowComments(rowData) {
-        // return (
-        //     <View style={{ borderRadius: 10 }}>
-        //         <TouchableOpacity style={styles.dataRow} onPress={() => this._rowPreseed(rowData.id)}>
-        //             <View style={styles.dataRowLeft}>
-        //                 <Text style={{ fontSize: 26 }}>{rowData.date.day}</Text>
-        //                 <Text style={{ fontSize: 13 }}>{rowData.date.month}</Text>
-        //             </View>
-        //             <View style={styles.dataRowRight}>
-        //                 <Text style={{ fontSize: 20 }}>{rowData.desc}</Text>
-        //             </View>
-        //         </TouchableOpacity>
-        //     </View>
-        // );
+        if (rowData.id == 1) {
+            return (
+                <View style={{ alignItems: 'flex-end', borderRadius: 10 }}>
+                    <View style={[styles.comment, styles.commentMine, { height: this.state.inputHeight }]}>
+                        <Text style={{ fontSize: 15 }}>{rowData.comment}
+                        </Text>
+                    </View>
+                </View>
+            );
+        } else {
+            return (
+                <View style={{ borderRadius: 10 }}>
+                    <View style={[styles.comment, styles.commentOther]}>
+                        <Text style={{ fontSize: 15 }}>{rowData.comment}</Text>
+                    </View>
+                </View>
+            );
+        }
     }
 
-    _renderRowPlayers(rowData) {
-        // return (
-        //     <View style={{ borderRadius: 10 }}>
-        //         <TouchableOpacity style={styles.dataRow} onPress={() => this._rowPreseed(rowData.id)}>
-        //             <View style={styles.dataRowLeft}>
-        //                 <Text style={{ fontSize: 26 }}>{rowData.date.day}</Text>
-        //                 <Text style={{ fontSize: 13 }}>{rowData.date.month}</Text>
-        //             </View>
-        //             <View style={styles.dataRowRight}>
-        //                 <Text style={{ fontSize: 20 }}>{rowData.desc}</Text>
-        //             </View>
-        //         </TouchableOpacity>
-        //     </View>
-        // );
+    _renderRowPlayersAccepted(rowData) {
+        return (
+            <View style={{ borderRadius: 10 }}>
+                <View style={styles.playerAccepted}>
+                    <Text style={{ fontSize: 15 }}>{rowData.id}</Text>
+                </View>
+            </View>
+        );
+    }
+
+    _renderRowPlayersWaiting(rowData) {
+        return (
+            <View style={{ borderRadius: 10 }}>
+                <View style={styles.playerWaiting}>
+                    <Text style={{ fontSize: 15 }}>{rowData.id}</Text>
+                </View>
+            </View>
+        );
     }
 }
 
@@ -143,26 +159,45 @@ var styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#d9d9d9',
-        flexDirection: 'row'
+        flexDirection: 'column'
     },
-    panelRight: {
-        width: 60
-    },
-    panelLeft: {
-        flex: 1
-    },
-    sectionLeft: {
+    sectionPlayers: {
         flex: 1,
         marginTop: 6,
+        marginBottom: 3,
+        marginRight: 6,
         marginLeft: 6,
         borderBottomWidth: 0.5,
         backgroundColor: '#F6F6F6',
         borderRadius: 5
     },
-    sectionRight: {
+    sectionMessages: {
         flex: 1,
-        alignItems: 'flex-start',
-        justifyContent: 'flex-end'
+        borderBottomWidth: 0.5,
+        backgroundColor: 'transparent',
+        borderRadius: 5
+    },
+    sectionForReading: {
+        flex: 1,
+        marginTop: 6,
+        marginBottom: 1,
+        marginRight: 6,
+        marginLeft: 6,
+        borderBottomWidth: 0.5,
+        backgroundColor: '#F6F6F6',
+        borderRadius: 5
+    },
+    sectionForWritting: {
+        height: 40,
+        marginTop: 1,
+        marginBottom: 6,
+        marginRight: 6,
+        marginLeft: 6,
+        borderBottomWidth: 0.5,
+        backgroundColor: '#F6F6F6',
+        borderRadius: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     button: {
         width: 50,
@@ -170,12 +205,61 @@ var styles = StyleSheet.create({
         backgroundColor: 'transparent',
         borderRadius: 5
     },
+    buttonInvitations: {
+        width: 50,
+        height: 50,
+    },
     buttonImage: {
-        width: 40,
-        height: 40,
-        position: 'absolute',
-        left: 7,
-        bottom: 7
+        width: 30,
+        height: 30,
+        position: 'relative'
+    },
+    sendComment: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    listViewPlayers: {
+        flex: 1,
+        flexDirection: 'row',
+        borderColor: 'grey'
+    },
+    listView: {
+        flex: 1,
+        borderColor: 'grey'
+    },
+    playerAccepted: {
+        marginTop: 10,
+        marginHorizontal: 6,
+        borderBottomWidth: 0.5,
+        height: 90,
+        width: 90,
+        borderRadius: 45,
+        backgroundColor: 'gray'
+    },
+    playerWaiting: {
+        marginTop: 10,
+        marginHorizontal: 6,
+        borderBottomWidth: 0.5,
+        height: 90,
+        width: 90,
+        borderRadius: 45,
+        backgroundColor: 'blue'
+    },
+    comment: {
+        marginTop: 10,
+        marginHorizontal: 6,
+        borderBottomWidth: 0.5,
+        height: 30,
+        borderRadius: 5,
+    },
+    commentOther: {
+        backgroundColor: 'green',
+        width: Dimensions.get('window').width * 0.70,
+    },
+    commentMine: {
+        marginRight: 10,
+        backgroundColor: 'yellow',
+        width: Dimensions.get('window').width * 0.70,
     }
 });
 
