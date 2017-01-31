d8316f925ca979d481649d0b9842fa259312959c
Facundo Cacho <facucacho10@gmail.com>
2017-01-31 11:01:39 -0300

    new manager screen added

---
+++ src/components/managerHome/ManagerHome.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+
+export default class ManagerHome extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    flexDirection: 'column',
+  }
+});
